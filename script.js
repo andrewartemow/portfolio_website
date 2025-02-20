04a3f7d21859deb42d11fe78fcf0a8b64959bde2
andrei <andrei@clinicsoftware.com>
2025-02-20 06:12:28 +0200

    update

--- script.js
+++ script.js
@@ -1,54 +1,59 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const toggleMenu = () => {
+    const menu = document.querySelector('.menu-links');
+    const icon = document.querySelector('.hamburger-icon');
 
-const toggleMenu = () => {
-  const menu = document.querySelector('.menu-links');
-  const icon = document.querySelector('.hamburger-icon');
+    menu.classList.toggle('open');
+    icon.classList.toggle('open');
 
-  menu.classList.toggle('open');
-  icon.classList.toggle('open');
-};
+    console.log('click');
+  };
 
-const SENSITIVITY = 10;
-const COOKIE_NAME = 'exit-popup';
+  //   const SENSITIVITY = 10;
+  //   const COOKIE_NAME = 'exit-popup';
 
-    const modalEle = document.getElementById('modal');
-    const modalOverlay = document.getElementById('modal-overlay');
-    const closeButton = document.getElementById('closeButton');
+  //   const modalEle = document.getElementById('modal');
+  //   const modalOverlay = document.getElementById('modal-overlay');
+  //   const closeButton = document.getElementById('closeButton');
 
-    const hasModalShown = () => {
-        const cookie = `; ${document.cookie}`.split(`; ${COOKIE_NAME}=`).pop().split(';').shift();
-        return cookie === 'true';
-    };
+  //   const hasModalShown = () => {
+  //     const cookie = `; ${document.cookie}`
+  //       .split(`; ${COOKIE_NAME}=`)
+  //       .pop()
+  //       .split(';')
+  //       .shift();
+  //     return cookie === 'true';
+  //   };
 
-    const markModalShown = () => {
-        const date = new Date();
-        date.setTime(date.getTime() + 1*24*60*60*1000);
-        document.cookie = `COOKIE_NAME=true; expires=${date.toUTCString()}`;
-    };
+  //   const markModalShown = () => {
+  //     const date = new Date();
+  //     date.setTime(date.getTime() + 1 * 24 * 60 * 60 * 1000);
+  //     document.cookie = `COOKIE_NAME=true; expires=${date.toUTCString()}`;
+  //   };
 
-    const closeModal = () => {
-        modalEle.classList.remove('modal--visible');
-        modalEle.classList.add('modal--hidden');
+  //   const closeModal = () => {
+  //     modalEle.classList.remove('modal--visible');
+  //     modalEle.classList.add('modal--hidden');
 
-        modalOverlay.classList.remove('modal--visible');
-        modalOverlay.classList.add('modal--hidden');
-    };
+  //     modalOverlay.classList.remove('modal--visible');
+  //     modalOverlay.classList.add('modal--hidden');
+  //   };
 
-    const handleMouseLeave = (e) => {
-        if (e.clientY < SENSITIVITY && !hasModalShown()) {
-            document.removeEventListener('mouseleave', handleMouseLeave);
+  //   const handleMouseLeave = (e) => {
+  //     if (e.clientY < SENSITIVITY && !hasModalShown()) {
+  //       document.removeEventListener('mouseleave', handleMouseLeave);
 
-            // Show the modal
-            modalEle.classList.remove('modal--hidden');
-            modalEle.classList.add('modal--visible');
+  //       // Show the modal
+  //       modalEle.classList.remove('modal--hidden');
+  //       modalEle.classList.add('modal--visible');
 
-            modalOverlay.classList.remove('modal--hidden');
-            modalOverlay.classList.add('modal--visible');
+  //       modalOverlay.classList.remove('modal--hidden');
+  //       modalOverlay.classList.add('modal--visible');
 
-            // markModalShown();
-        }
-    };
+  //       // markModalShown();
+  //     }
+  //   };
 
-    document.addEventListener('mouseleave', handleMouseLeave);
-    closeButton.addEventListener('click', closeModal);
+  //   document.addEventListener('mouseleave', handleMouseLeave);
+  //   closeButton.addEventListener('click', closeModal);
 });
